cba503f0451bf339899ccc8670e8f2769b7c2e37
Leo Mogiano <leomogiano@outlook.com>
2024-11-01 08:04:38 -0400

    Initial Commit

---
+++ MangaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MangaApp
+//
+//  Created by Leo Mogiano on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MangaApp/MangaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaAppApp.swift
+//  MangaApp
+//
+//  Created by Leo Mogiano on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
